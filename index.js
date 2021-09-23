221feca112be8b84e4c0a2c1eccaad15d5e2e6dc
shearertzach <shearertzach@gmail.com>
2021-09-22 22:23:54 -0500

    Completed

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let usernames = {}
+let numUsers = 0
+
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
@@ -12,11 +15,34 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('send message', (msg) => {
-    io.emit('recieve message', msg);
-  });
+  var addedUser = false;
+
+  socket.on('add user', (username) => {
+    socket.username = username
+
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  })
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      socket.broadcast.emit('user disconnected', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', data);
   });
 });
 
